1a4ad8b1776584d41573e12c6dffe6f7464c3c1a
vaibhavsraut <vaibhav.raut851@gmail.com>
2024-01-07 11:24:44 +0530

    Display Welcome message

---
+++ src/com/employeewage/EmployeeWageMain.java
@@ -0,0 +1,7 @@
+package com.employeewage;
+public class EmployeeWageMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Program");
+    }
+}
+
