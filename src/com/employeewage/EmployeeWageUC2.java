77e1bd8c8afcb11cd04ab0f0c5e7fe5b507ca489
vaibhavsraut <vaibhav.raut851@gmail.com>
2024-01-07 11:33:33 +0530

    Implemented UC2: Calculate Daily Employee Wage

---
+++ src/com/employeewage/EmployeeWageUC2.java
@@ -0,0 +1,12 @@
+package com.employeewage;
+public class EmployeeWageUC2 {
+    public static void main(String[] args) {
+        int wagePerHour = 20;
+        int fullDayHour = 8;
+
+        int dailyWage = wagePerHour * fullDayHour;
+
+        System.out.println("Daily Employee Wage: " + dailyWage);
+    }
+}
+
